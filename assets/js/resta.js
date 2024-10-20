b6c93653a7840d87675407c27bf8a1ee6f743aa5
axelmaan <axel.garrido.a@gmail.com>
2024-10-20 17:15:58 -0300

    orden

--- assets/js/resta.js
+++
@@ -1,3 +0,0 @@
-valor1 = 230
-valor2 = 120
-alert (valor1 - valor2)
--- assets/js/scriptdom.js
+++
@@ -1,6 +0,0 @@
-//se solicita
-borde = prompt ("ingrese borde")
-//seleccionamos que modificar
-txtborde = document.querySelector ("img")
-//modificamos
-txtborde.style.borderRadius = borde
